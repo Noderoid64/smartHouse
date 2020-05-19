b6712c2c91ad6a7d7b9e806ca91ff563c3b4e7f2
noderoid64 <noderoid64@gmail.com>
2020-05-19 22:50:50 +0300

    [Bug] fix connect device to network dialog

--- android_client/lib/widgets/add-new-device-icon/new-device-dialog/new-device-dialog.viewmodel.dart
+++ android_client/lib/widgets/add-new-device-icon/new-device-dialog/new-device-dialog.viewmodel.dart
@@ -3,9 +3,13 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../../../services/web/device-rest.service.dart';
+
 class NewDeviceDialogViewModel extends ChangeNotifier {
   GlobalKey formKey = GlobalKey<FormState>();
   final ssidController = TextEditingController();
+  final deviceRestService = new DeviceRestService();
+  String ssid;
 
   String ssidValidatorFunction(String value) {
     if (value.isEmpty) {
@@ -16,8 +20,16 @@ class NewDeviceDialogViewModel extends ChangeNotifier {
     return null;
   }
 
-  void submitButtonPressed() {
-    log(ssidController.value.text);
+  void getSsid (String ssid) {
+    this.ssid = ssid;
+  }
+
+  void submitButtonPressed() async {
+
+    String pass = ssidController.value.text;
+    log(ssid + " " + pass);
+    bool good = await deviceRestService.login(ssid, pass);
+    log(good.toString());
   }
 
   @override
